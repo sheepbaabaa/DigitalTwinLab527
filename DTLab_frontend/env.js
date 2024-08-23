b2eabd6fdf4cd4ac99ecd34c4a0bbdcafce46a60
Mahaotian <maht2001@163.com>
2024-08-23 11:32:05 +0800

    update:gitignore

---
+++ DTLab_frontend/env.js
@@ -0,0 +1,5 @@
+//const HOST = "dtlab.qylh.xyz:9003";
+// const HOST = "dtlab.qylh.xyz:9003";
+const HOST = "localhost:8080";
+const MODEL_LOCATION=0; //设置为0则本地读取，设置为1则远程读取
+export { HOST, MODEL_LOCATION }; 
