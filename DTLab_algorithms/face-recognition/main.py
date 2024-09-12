7ee1a7cec2e57f1eb76f0811b46dca64740ecc98
mahaotian's debian <maht2001@163.com>
2024-09-12 11:16:12 +0800

    update face recognition

--- DTLab_algorithms/face-recognition/main.py
+++ DTLab_algorithms/face-recognition/main.py
@@ -16,27 +16,25 @@ names = data['names']
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
+
 mqtt_client = MqttClient("face_recognizer", "127.0.0.1", 1883, "dtlab", "527")
 while True:
     ret, frame = video_capture.read()
-    if process_this_frame:
-        small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        new = rgb_small_frame.copy()
-        face_encodings = face_recognition.face_encodings(new, face_locations)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(faces, face_encoding, )
-            name = "Unknown"
-            face_distances = face_recognition.face_distance(faces, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = names[best_match_index]
-                mqtt_client.publish("face_recognize_result", {"name":name, "time":time.time(), "distance":face_distances[best_match_index]})
-            face_names.append(name)
-    process_this_frame = not process_this_frame
+    small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    new = rgb_small_frame.copy()
+    face_encodings = face_recognition.face_encodings(new, face_locations)
+    face_names = []
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(faces, face_encoding, )
+        name = "Unknown"
+        face_distances = face_recognition.face_distance(faces, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = names[best_match_index]
+            mqtt_client.publish("face_recognize_result", {"name":name, "time":time.time(), "distance":face_distances[best_match_index]})
+        face_names.append(name)
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         cv2.rectangle(frame, (left-10, top-10), (right+10, bottom+10), (0, 0, 255), 1)
         font = cv2.FONT_HERSHEY_DUPLEX
