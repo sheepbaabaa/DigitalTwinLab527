0267c4b57ee7ab2c57b14d938aff3f7d2d6556c6
mahaotian's debian <maht2001@163.com>
2024-11-05 14:56:21 +0800

    Add release function

--- DTLab_algorithms/hikvision/hikvision_demo.py
+++ DTLab_algorithms/hikvision/hikvision_demo.py
@@ -4,10 +4,16 @@ from hikvision.camera_controller import CameraController
 
 controller = CameraController()
 
-ptz = controller.get_ptz()
-ptz.pan -= 100
-controller.set_ptz(ptz)
-# while True:
-#     img = controller.get_frame()
-#     cv2.imshow('camera',img)
-#     cv2.waitKey(1)
+# ptz = controller.get_ptz()
+# ptz.pan -= 100
+# controller.set_ptz(ptz)
+while True:
+    # 读取摄像头的当前帧
+    ret, frame = controller.get_frame()
+    # 如果帧读取成功
+    if ret:
+        # 在窗口中显示帧
+        cv2.imshow("TpLink IPC Camera", frame)
+    # 按下 'ESC' 退出
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
